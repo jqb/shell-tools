ef73045e37eb65103c2202bece28a5dba6855498
Kuba Janoszek <kuba.janoszek@gmail.com>
2012-06-11 15:59:01 +0200

    files encoding changed

--- posix-include.sh
+++ posix-include.sh
@@ -10,11 +10,6 @@
 # export JBOSS_HOME=~/docs/library/java/jboss-5.1.0.GA
 # export PATH=$JVM:$PATH
 
-# Tomcat - catalina
-export CATALINA_HOME='/c/Program Files (x86)/Apache Software Foundation/Tomcat 6.0'
-export CATALINA_TMPDIR="$CATALINA_HOME/temp"
-export CLASSPATH=$CLASSPATH:$(ls "$CATALINA_HOME/lib"/*.jar | tr '\n' ':')
-
 # grails & groovy
 # export GRAILS_HOME=~/.grails
 # PATH=$GRAILS_HOME/bin:$PATH
@@ -25,10 +20,6 @@ export CLASSPATH=$CLASSPATH:$(ls "$CATALINA_HOME/lib"/*.jar | tr '\n' ':')
 # export GOARCH=386
 # export GOBIN=$HOME/.go/bin
 # PATH=$GOBIN:$PATH
-
-# 7z
-export _7Z_HOME='/c/Program Files/7-Zip'
-export PATH=$PATH:"$_7Z_HOME"
 
 # maven
 # export MVN2_HOME='/usr/local/apache-maven/apache-maven-2.0.9'
