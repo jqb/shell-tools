20b6693781e1b11d28973d7ccb707ce7b69d5c0c
Kuba Janoszek <kuba.janoszek@gmail.com>
2013-02-11 19:40:57 +0100

    windows: Tomcat path removed since I do not have it anymore

--- nt-include.sh
+++ nt-include.sh
@@ -1,9 +1,9 @@
 # ENVIRONMENT #################################################
 
 # Tomcat - catalina
-export CATALINA_HOME='/c/Program Files (x86)/Apache Software Foundation/Tomcat 6.0'
-export CATALINA_TMPDIR="$CATALINA_HOME/temp"
-export CLASSPATH=$CLASSPATH:$(ls "$CATALINA_HOME/lib"/*.jar | tr '\n' ':')
+# export CATALINA_HOME='/c/Program Files (x86)/Apache Software Foundation/Tomcat 6.0'
+# export CATALINA_TMPDIR="$CATALINA_HOME/temp"
+# export CLASSPATH=$CLASSPATH:$(ls "$CATALINA_HOME/lib"/*.jar | tr '\n' ':')
 
 # 7z
 export _7Z_HOME='/c/Program Files/7-Zip'
