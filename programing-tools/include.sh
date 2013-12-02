e8b857a17513f888fbe4e3559cf29c8832ed47e8
jqb <kuba.janoszek@gmail.com>
2013-12-02 20:59:37 +0100

    New autoenv

--- programing-tools/include.sh
+++ programing-tools/include.sh
@@ -25,3 +25,35 @@ function set_git_sensitive_prompt() {
 
 set_git_sensitive_prompt
 
+
+# hg sensitive prompt
+# hg_branch() {
+#     hg branch 2> /dev/null | \
+#         awk '{ printf "\033[37;0m on \033[35;40m" $1 }'
+#     hg bookmarks 2> /dev/null | \
+#         awk '/\*/ { printf "\033[37;0m at \033[33;40m" $2 }'
+# }
+
+# function set_hg_sensitive_prompt() {
+#     DEFAULT="[37;40m"
+#     PINK="[35;40m"
+#     GREEN="[32;40m"
+#     ORANGE="[33;40m"
+#     PS1='\w\$(hg_branch)\e${GREEN}\e${DEFAULT}$ '
+# }
+
+
+function cd () {
+    builtin cd $@ && eval "$(autovirtualenv_command)"
+}
+
+function db () {
+    if [ -n "$MYSQL_CONFIG_FILE" ]; then
+        echo -e "\n"
+        echo -e "    Running mysql client, config file: $MYSQL_CONFIG_FILE"
+        echo -e "\n"
+        mysql --defaults-file=$MYSQL_CONFIG_FILE $@
+    else
+        mysql $@
+    fi
+}
