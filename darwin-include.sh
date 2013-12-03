0f2f3094c4a25c1be9ed83003fc88640c3eecb63
jqb <kuba.janoszek@gmail.com>
2013-12-03 18:25:59 +0100

    separate mac configuration added

--- darwin-include.sh
+++ darwin-include.sh
@@ -9,3 +9,12 @@ export ARCHFLAGS="-arch x86_64"
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
 source /usr/local/bin/virtualenvwrapper.sh
+
+
+# Mac iTerm2 hints:
+# 1) cmd + delete -> kill word
+#    http://felipecsl.com/blog/2012/06/05/protip-delete-words-on-iterm2-mac-osx/
+#    hex sequence: 0x1b 0x08
+#
+# 2) word forward / backward: cmd + shift + F, cmd + shift + B
+#    http://superuser.com/questions/357355/how-can-i-get-controlleft-arrow-to-go-back-one-word-in-iterm2
