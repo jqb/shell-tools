673a4eec0957ae25994c233d1894d650b8f698f1
jqb <kuba.janoszek@gmail.com>
2013-12-10 22:55:59 +0100

    another old template

--- programing-tools/programing-tools.sh
+++ programing-tools/programing-tools.sh
@@ -5,19 +5,6 @@ function svn-add-all(){
 # END OF SVN ########################################
 
 
-
-# BASH #############################################
-function create-simple-bash-project(){
-    projectName=$1
-    mkdir $projectName && cd $projectName && touch $projectName.sh
-    echo -e "#!/bin/bash\n" > $projectName.sh
-    chmod +x $projectName.sh
-    rtmp
-    emacs $projectName.sh
-}
-# END OF BASH ######################################
-
-
 # CLOJURE ##########################################
 function clojure() {
     java -cp $TOOLS/programing-tools/clojure-1.3.0.jar clojure.main $@
