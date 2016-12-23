53ad108de309158b8a6c37714048b620bba02f19
jqb <kuba.janoszek@gmail.com>
2016-12-23 14:46:50 +0100

    penv-plugins:docker_plugin_on_deactivate: avoiding unset* errors

--- penv-plugins/docker_plugin_on_deactivate.sh
+++ penv-plugins/docker_plugin_on_deactivate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-unset -f show-containers;
-unset -f docker-ps-filter-data-containers;
-unset -f rm-containers;
-unset -f stop-containers;
-unset -f inspect-container;
-unset -f show-data;
-unset -f dump-database;
-unset -f attach-to;
+unset -f show-containers >/dev/null 2>&1;
+unset -f docker-ps-filter-data-containers >/dev/null 2>&1;
+unset -f rm-containers >/dev/null 2>&1;
+unset -f stop-containers >/dev/null 2>&1;
+unset -f inspect-container >/dev/null 2>&1;
+unset -f show-data >/dev/null 2>&1;
+unset -f dump-database >/dev/null 2>&1;
+unset -f attach-to >/dev/null 2>&1;
