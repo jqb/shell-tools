7be65a017d6292d4e50559d7dd6d036b3eee746a
jqb <kuba.janoszek@gmail.com>
2018-07-27 19:52:06 +0200

    penv-plugins: "monitor_files_plugin_on_activate.sh" some better monitoring

--- penv-plugins/monitor_files_plugin_on_activate.sh
+++ penv-plugins/monitor_files_plugin_on_activate.sh
@@ -8,10 +8,13 @@ function monitor-files () {
             if [[ "$filename" == *_flymake.py ]]; then
                 continue
             fi
-            if [[ "$filename" == .#*.py || "$filename" == .#*.clj ]]; then
+            if [[ "$filename" == *.pyc ]]; then
                 continue
             fi
-            if [[ "$filename" == *.penv* ]]; then
+            if [[ "$filename" =~ .*_flymake.py ]]; then
+                continue
+            fi
+            if [[ "$filename" == .#* ]]; then
                 continue
             fi
             if [[ "$filename" == *-journal ]]; then  # SQLite crap
@@ -20,13 +23,134 @@ function monitor-files () {
             if [[ "$filename" == *db ]]; then  # SQLite crap
                 continue
             fi
-            # echo "# ===> $directory$filename"
-            if [[ "$filename" == *.py || "$filename" == *.clj ]]; then
-                # echo "# ===> $directory$filename"
-                $@
-                echo "DONE: $(date +%Y-%m-%d__%H:%M:%S)"
+            if [[ "$directory" =~ .*.git ]]; then
+                continue
             fi
+            if [[ "$directory" =~ .*.penv ]]; then
+                continue
+            fi
+            if [[ "$directory" =~ .*__pycache__ ]]; then
+                continue
+            fi
+            if [[ "$directory" =~ .*.htmlcov ]]; then
+                continue
+            fi
+            if [[ "$directory" =~ .*dist ]]; then  # typescript tryouts // should be defineable in directory
+                continue
+            fi
+            if [[ "$directory" =~ .*node_modules ]]; then
+                continue
+            fi
+            # echo "# ===> $directory$filename"
+            # if [[ "$filename" == *.py || "$filename" == *.clj ]]; then
+            echo "# ==> $directory$filename"
+            export FILE_MONITOR_CHANGED_FILEPATH="$directory$filename"
+            export FILE_MONITOR_CHANGED_FILENAME="$filename"
+            export FILE_MONITOR_CHANGED_FILEPATH_RELATIVE="$(realpath --relative-to "$PWD" "$FILE_MONITOR_CHANGED_FILEPATH")"
+            echo "# ================================================="
+            time $@
+            echo "# ================================================="
+            echo "DONE: $(date +%Y-%m-%d__%H:%M:%S)"
+            # fi
         done
+}
+
+
+# function sync-cua-with-server () {
+#     (cd /home/xubuntu-vm/projects/swisscom_workspace/swisscom/projects && \
+#             rsync -r -og --chown=cuadev:cuadev --safe-links --executability \
+#                   --exclude "*.pyc" \
+#                   --exclude ".penv/" \
+#                   --exclude ".git/" \
+#                   --exclude "cua-project/tmp/*" \
+#                   --exclude "cua-project/.sass-cache" \
+#                   --exclude "cua-project/.htmlcov" \
+#                   --exclude "__pycache__" \
+#                   cua-project cua-client cua-common ibe-tablib django-sortedm2m cuadev@jqbdev01:/home/cuadev/sync
+#     )  # end of sub-shell
+# }
+
+
+function sync-ibe-with-server () {
+    # --exclude ".git/" \
+    (cd /home/xubuntu-vm/projects/swisscom_workspace/ && \
+            rsync -r -og --safe-links --executability \
+                  --exclude "*.pyc" \
+                  --exclude ".penv/" \
+                  --exclude "*_flymake.py" \
+                  --exclude "*.ibe-platform.ch__regeneration_only.log" \
+                  --exclude "*.recent.txt" \
+                  --exclude "env" \
+                  --exclude "environments_configuration" \
+                  --exclude "projects/cua-project/tmp/" \
+                  --exclude "projects/cua-project/.sass-cache" \
+                  --exclude "projects/cua-project/.htmlcov" \
+                  --exclude "projects/blackbox" \
+                  --exclude "projects/envs_sync" \
+                  --exclude "projects/importanalyser" \
+                  --exclude "projects/obfuscator" \
+                  --exclude "projects/sync-cuadev" \
+                  --exclude "__pycache__" \
+                  swisscom/* swisscom/.* root@jqbdev01:/home/dockerapps/ibe \
+            && \
+            ssh -t root@jqbdev01 'chown -R dockerapps:dockerapps /home/dockerapps/ibe' \
+            && \
+            echo "DONE"
+    )  # end of sub-shell
+}
+
+
+function sync-cua-with-server () {
+    # --exclude ".git/" \
+    # --exclude "tmp/data/dumps" \
+    (cd /home/xubuntu-vm/projects/swisscom_workspace/swisscom/projects && \
+            rsync -r -og --safe-links --executability \
+                  --exclude "*.pyc" \
+                  --exclude ".penv/" \
+                  --exclude "*_flymake.py" \
+                  --exclude "tmp" \
+                  --exclude ".sass-cache" \
+                  --exclude ".htmlcov" \
+                  --exclude "__pycache__" \
+                  cua-project/* cua-project/.* root@jqbdev01:/home/dockerapps/ibe/projects/cua-project \
+            && \
+            ssh -t root@jqbdev01 'chown -R dockerapps:dockerapps /home/dockerapps/ibe/projects/cua-project' \
+            && \
+            echo "DONE"
+    )  # end of sub-shell
+}
+
+
+function sync-blocks-with-server () {
+    (cd /home/xubuntu-vm/projects/ && \
+            rsync -r -og --chown=dockerapps:dockerapps --safe-links --executability \
+                  --exclude "*.pyc" \
+                  --exclude "*_flymake.py" \
+                  --exclude ".penv/" \
+                  --exclude "__pycache__" \
+                  blocks_project/* blocks_project/.* root@jqbdev01:/home/dockerapps/blocksapp \
+            && \
+            ssh -t root@jqbdev01 'chown -R dockerapps:dockerapps /home/dockerapps/blocksapp' \
+            && \
+            ssh -t root@jqbdev01 'cp -r /home/dockerapps/blocksapp/static_collected /home/dockerapps/blocksapp_static/static && chown -R dockerapps:www-data /home/dockerapps/blocksapp_static'
+
+
+    )  # end of sub-shell
+}
+
+
+function sync-static-blocks-with-server () {
+    (cd /home/xubuntu-vm/projects/sandbox/Blocks/ && \
+            rsync -r -og --chown=dockerapps:www-data --safe-links --executability \
+                  --exclude "*.pyc" \
+                  --exclude ".penv/" \
+                  --exclude "*_flymake.py" \
+                  --exclude "__pycache__" \
+                  --exclude "node_modules" \
+                  index.html dist/* style root@jqbdev01:/home/dockerapps/blocksapp_static \
+            && \
+            ssh -t root@jqbdev01 'chown -R dockerapps:www-data /home/dockerapps/blocksapp_static'
+    )  # end of sub-shell
 }
 
 
